a5dba61446eaf5734339383056d896368c0a20a8
Ben <benphillips96@outlook.com>
2022-05-13 13:22:11 +0100

    includes presentation

--- src/main/java/com/qa/ims/persistence/domain/order.java
+++ src/main/java/com/qa/ims/persistence/domain/order.java
@@ -4,85 +4,85 @@ import java.util.List;
 import java.util.Objects;
 
 public class Order {
-	
-	private Long id;
-	private List<Item> orderItems;
-	private Long customerID;
-	
-	
+		
+		private Long id;
+		private List<Item> orderItems;
+		private Long customerID;
+		
+		
 
-	public Order(Long id, List<Item> orderItems, Long customerID) {
-		super();
-		this.id = id;
-		this.orderItems = orderItems;
-		this.customerID = customerID;
-	}
+		public Order(Long id, List<Item> orderItems, Long customerID) {
+			super();
+			this.id = id;
+			this.orderItems = orderItems;
+			this.customerID = customerID;
+		}
 
-	public Order(Long customerID) {
-		super();
-		this.customerID = customerID;
-	}
-	
-	public Order(List<Item> orderItems, Long customerID) {
-		super();
-		this.orderItems = orderItems;
-		this.customerID = customerID;
-	}
+		public Order(Long customerID) {
+			super();
+			this.customerID = customerID;
+		}
+		
+		public Order(List<Item> orderItems, Long customerID) {
+			super();
+			this.orderItems = orderItems;
+			this.customerID = customerID;
+		}
 
-	public Order(Long id, Long customerID) {
-		super();
-		this.id = id;
-		this.customerID = customerID;
-	}
+		public Order(Long id, Long customerID) {
+			super();
+			this.id = id;
+			this.customerID = customerID;
+		}
 
-	public Long getID() {
-		return id;
-	}
+		public Long getID() {
+			return id;
+		}
 
-	public void setID(Long id) {
-		this.id = id;
-	}
+		public void setID(Long id) {
+			this.id = id;
+		}
 
-	public List<Item> getOrderItems() {
-		return orderItems;
-	}
+		public List<Item> getOrderItems() {
+			return orderItems;
+		}
 
-	public void setOrderItems(List<Item> orderItems) {
-		this.orderItems = orderItems;
-	}
+		public void setOrderItems(List<Item> orderItems) {
+			this.orderItems = orderItems;
+		}
 
-	public Long getCustomerID() {
-		return customerID;
-	}
+		public Long getCustomerID() {
+			return customerID;
+		}
 
-	public void setCustomerID(Long customerID) {
-		this.customerID = customerID;
-	}
+		public void setCustomerID(Long customerID) {
+			this.customerID = customerID;
+		}
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(customerID, id, orderItems);
-	}
+		@Override
+		public int hashCode() {
+			return Objects.hash(customerID, id, orderItems);
+		}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Order other = (Order) obj;
-		return Objects.equals(customerID, other.customerID) && Objects.equals(id, other.id)
-				&& Objects.equals(orderItems, other.orderItems);
-	}
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Order other = (Order) obj;
+			return Objects.equals(customerID, other.customerID) && Objects.equals(id, other.id)
+					&& Objects.equals(orderItems, other.orderItems);
+		}
 
-	@Override
-	public String toString() {
-		return "Order [orderID=" + id + ", orderItems=" + orderItems + ", customerID=" + customerID + "]";
-	}
-	
-	
-	
-	
-	}
+		@Override
+		public String toString() {
+			return "Order [orderID=" + id + ", orderItems=" + orderItems + ", customerID=" + customerID + "]";
+		}
+		
+		
+		
+		
+		}
